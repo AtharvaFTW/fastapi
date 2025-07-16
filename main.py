64d68505a9242c9680cc3ec4f8c2f73ac0115b48
bluefoxuw <shreyuchisoumya@gmail.com>
2025-07-16 21:07:51 +0530

    router

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from fastapi import FastAPI
-import db
+from fastapi import FastAPI,HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from api import page1_data,page2_reference,page3_generate,page4_slides
 
 app=FastAPI()
-@app.get("/")
-def read_root():
-    return {"Hello":"World"}
 
-@app.get("/api/courses")
-def get_all_courses():
-    courses=db.fetch_courses()
-    return {"Courses": courses}
+app.include_router(page1_data.router,prefix="/api/data", tags=["Page 1 -Data"])
+app.include_router(page2_reference.router, prefix="/api/reference", tags=["Page 2 - Reference Text"])
+app.include_router(page3_generate.router, prefix="/api/generate", tags=["Page 3 - Generate Content"])
+app.include_router(page4_slides.router, prefix="/api/slides", tags=["Page 4 - Slides"])
+
+p
